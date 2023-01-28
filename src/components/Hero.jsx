720eaf47e892fb80a752ef2c6431292b702825c9
Jack Ault <henlowgg@outlook.com>
2023-01-28 14:38:37 -0500

    adding more room for top section of application and ensuring app fits on all devices

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import styles from "../style";
+import { discount, robot } from "../assets";
 
 const Hero = () => (
-  <div>Hero</div>
+  <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+    <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+
+    </div>
+  </section>
 )
 
 export default Hero
