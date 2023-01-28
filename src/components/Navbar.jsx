32f90b21fdc8a96fe7b828858276b7dc13a23453
Jack Ault <henlowgg@outlook.com>
2023-01-28 14:27:11 -0500

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,9 +12,8 @@ const Navbar = () => {
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${
-              index === navLinks.length - 1 ? "mr-0" : "mr-10"
-            } text-white`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] 
+            ${index === navLinks.length - 1 ? "mr-0" : "mr-10"} text-white`}
           >
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
@@ -34,12 +33,12 @@ const Navbar = () => {
             toggle ? "flex" : "hidden"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+          <ul className="list-none flex flex-col justify-end items-center flex-1">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
                 className={`font-poppins font-normal cursor-pointer text-[16px] ${
-                  index === navLinks.length - 1 ? "mr-0" : "mr-10"
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
                 } text-white`}
               >
                 <a href={`#${nav.id}`}>{nav.title}</a>
