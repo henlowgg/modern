b03e4c4eec279f2f25a0ed06a936bdd8ed088363
Jack Ault <henlowgg@outlook.com>
2023-02-03 13:20:37 -0500

    added gradients and additional styling behind img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,9 +28,21 @@ const Hero = () => (
       <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
         Payment Method.
       </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates and annual fees.</p>
-
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Our team of experts uses a methodology to identify the credit cards most likely to fit your needs. We examine annual percentage rates and annual fees.
+        </p>
     </div>
+
+    {/* left side of screen completed above ^^ */}
+    {/* right side of screen below vv */}
+
+    <div>
+      <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+    </div>
+
+
   </section>
 )
 
