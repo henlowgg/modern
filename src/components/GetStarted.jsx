db7d719fc5ff8b9839f53b6495ebb7f94dacb5bc
Jack Ault <henlowgg@outlook.com>
2023-02-03 12:58:18 -0500

    finished adding payment area p text

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -9,7 +9,7 @@ const GetStarted = () => (
         <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
           <span className="text-gradient">Get</span>  
         </p>
-          <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain"/>
+        <img src={arrowUp} alt="arrow" className="w-[23px] h-[23px] object-contain"/>
 
       </div>
 
