af3a0953a05a40ccf10b013e0dc539174542b1a6
Jack Ault <henlowgg@outlook.com>
2023-02-04 14:51:52 -0500

    starting on dynamic sections for the business area which can be used in future development for different apps

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { features } from "../constants"
+import styles, { layout } from "../style"
+import Button from './Button';
 
 const Business = () => {
   return (
-    <div>Business</div>
+    <section id="features" className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          You do the business, <br className="sm:block hidden" /> we'll handle the moneys honey.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          With the right credit card, you can improve your home life by buying stuff you dont need. But with hundreds of credit cards on the market.
+        </p>
+      </div>
+
+    </section>
   )
 }
 
