01b66c5066fe8c354752a808a5592be409986cef
Jack Ault <henlowgg@outlook.com>
2023-02-04 15:31:40 -0500

    adding more information for cards and finishing up styling them, having some fun with it

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient rounded font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient rounded-[15px] font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
       Get Started
     </button>
   )
